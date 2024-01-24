c5e6463b0b2ef18b07c048fedb2332160d823671
Ayush Kumar Upadhyay <115970349+Upadhyayayush30@users.noreply.github.com>
2024-01-24 23:26:06 +0530

    Time: 248 ms (87.67%) | Memory: 176.9 MB (99.46%) - LeetSync

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+/*class Solution {
 public:
     int pseudoPalindromicPaths(TreeNode* root) {
         return countPalindromicPaths(root, 0);
@@ -30,6 +30,32 @@ private:
 
         int leftCount = countPalindromicPaths(node->left, path);
         int rightCount = countPalindromicPaths(node->right, path);
+
+        return leftCount + rightCount;
+    }
+};
+*/
+class Solution {
+public:
+    int pseudoPalindromicPaths(TreeNode* root) {
+        return countPseudoPalindromes(root, 0);
+    }
+
+private:
+    int countPseudoPalindromes(TreeNode* node, int path) {
+        if (node == nullptr) {
+            return 0;
+        }
+
+        path ^= (1 << node->val);
+
+        if (node->left == nullptr && node->right == nullptr) {
+            // Leaf node
+            return (path & (path - 1)) == 0 ? 1 : 0;
+        }
+
+        int leftCount = countPseudoPalindromes(node->left, path);
+        int rightCount = countPseudoPalindromes(node->right, path);
 
         return leftCount + rightCount;
     }
