98fcc95b88d60c34440642a8b318be9479888b98
Ayush Kumar Upadhyay <115970349+Upadhyayayush30@users.noreply.github.com>
2024-03-17 15:17:33 +0530

    Time: 17 ms (21.69%) | Memory: 20.5 MB (58.08%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,25 +2,24 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         vector<vector<int>> merged;
-        int n = intervals.size();
         int i = 0;
 
-        // Binary search to find the insertion point for newInterval
-        while (i < n && intervals[i][1] < newInterval[0]) {
+        // Add intervals that end before newInterval starts
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
             merged.push_back(intervals[i]);
             i++;
         }
 
-        // Merge newInterval with overlapping intervals
-        while (i < n && intervals[i][0] <= newInterval[1]) {
+        // Merge intervals that overlap with newInterval
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
             newInterval[0] = min(newInterval[0], intervals[i][0]);
             newInterval[1] = max(newInterval[1], intervals[i][1]);
             i++;
         }
         merged.push_back(newInterval);
 
-        // Merge remaining intervals with newInterval if necessary
-        while (i < n) {
+        // Add remaining intervals
+        while (i < intervals.size()) {
             merged.push_back(intervals[i]);
             i++;
         }
