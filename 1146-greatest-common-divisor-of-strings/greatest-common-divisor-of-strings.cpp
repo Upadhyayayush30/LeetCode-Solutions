980948433e23d0fc2fb5b3a3e5b07a921dfde966
Ayush Kumar Upadhyay <115970349+Upadhyayayush30@users.noreply.github.com>
2024-10-07 11:35:26 +0530

    Time: 3 ms (58.51%) | Memory: 8.5 MB (95.70%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -9,3 +9,29 @@ public:
         }
     }
 };
+/*
+APPROACH WITHOUT USING GCD FUNCTION AND CALCULATING MANUALLY
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        // Check if concatenation of str1+str2 equals str2+str1
+        if (str1 + str2 != str2 + str1) {
+            return ""; // If not, return empty string
+        }
+        
+        // Calculate GCD of lengths manually
+        int len1 = str1.length();
+        int len2 = str2.length();
+        
+        // Manually find the GCD using the Euclidean algorithm
+        while (len2 != 0) {
+            int temp = len2;
+            len2 = len1 % len2;
+            len1 = temp;
+        }
+        
+        // Return the substring from the beginning of str1 of length len1
+        return str1.substr(0, len1);
+    }
+};
+*/
