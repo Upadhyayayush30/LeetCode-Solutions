eafa303be1ca1a2886dceec9eb1546474801c09e
Ayush Kumar Upadhyay <115970349+Upadhyayayush30@users.noreply.github.com>
2024-10-08 13:40:21 +0530

    Time: 89 ms (96.05%) | Memory: 32.7 MB (79.33%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int minSwaps(string s) {
         stack<char>st;
@@ -12,5 +12,21 @@ public:
         }
         //size of stack == number of open brackets
         return (st.size()+1)/2;
+    }
+};*/
+class Solution {
+public:
+    int minSwaps(string s) {
+        int size=0;
+        for(char &ch: s){
+            if(ch=='['){
+                size++;
+            }
+            else if(size>0){
+                size--;//balancing the closing bracket ] with an open bracket in the stack
+            }
+        }
+        //size of stack == number of open brackets
+        return (size+1)/2;
     }
 };
